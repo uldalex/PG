65bf46ba9873ade2a1cbebfa2e576ea1fb9853c1
uldalex <uldalex@gmail.com>
2020-10-22 09:07:21 +0300

    блок ротации

--- src/js/script.js
+++ src/js/script.js
@@ -8,4 +8,20 @@
 // $( document ).ready(function() {});
 
 
-
+var practice = document.getElementById("practice");
+var branch = document.getElementById("branch");
+var children = [].slice.call(practice.children)
+var childrenBranch = [].slice.call(branch.children)
+                    setInterval(function() {
+                        var clone = children.slice();
+                        while (clone.length) {
+                            practice.append(clone.splice(Math.floor(Math.random() * clone.length), 1)[0]);
+                        }
+                    }, 6000);
+                    setInterval(function() {
+                        var clone = childrenBranch.slice();
+                        while (clone.length) {
+                            branch.append(clone.splice(Math.floor(Math.random() * clone.length), 1)[0]);
+                        }
+                    }, 5500);
+                   
