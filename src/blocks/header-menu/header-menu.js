b5b9891934c1f866a910706fc059c11589eec587
uldalex <uldalex@gmail.com>
2020-10-23 14:06:04 +0300

    модальные

--- src/blocks/header-menu/header-menu.js
+++ src/blocks/header-menu/header-menu.js
@@ -5,3 +5,17 @@
 // ready(function(){
 //   
 // });
+const ready = require('../../js/utils/documentReady.js');  
+ready(function(){
+    var language = document.getElementById('header-menu__language');
+    var languageList = document.getElementById('language-list');
+    var arrow = document.getElementById('arrow_down');
+    language.addEventListener('click', showlanguageTarget);
+   
+    function showlanguageTarget() {
+        languageList.classList.toggle('header-menu__language-list--open');
+        arrow.classList.toggle('arrow_down--open');
+        
+    }
+   
+    });
